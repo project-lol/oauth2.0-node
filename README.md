4b707b6f9980318a49497a481b1ecbe4ef30e6e9
노예찬 <71473074+noy3928@users.noreply.github.com>
2024-06-08 01:45:55 +0900

    chore : setting project

---
+++ README.md
@@ -0,0 +1,1 @@
+# From [Developing an OAuth 2.0 Provider for a Resource Server from Scratch](https://hackernoon.com/developing-an-oauth-20-provider-for-a-resource-server-from-scratch-3v6n3vcm)
---
+++ server/app.js
@@ -0,0 +1,35 @@
+//app.js
+const path = require("path");
+const bodyParser = require("body-parser");
+const express = require("express");
+
+const app = express();
+const publicPath = path.join(__dirname, "../public");
+const userRouter = require("./routes/UserRoutes");
+const projectRouter = require("./routes/ProjectRoutes");
+const OAuthRouter = require("./routes/OAuthRoutes");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(
+  express.static(publicPath, {
+    extensions: ["html"],
+  })
+);
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Expose-Headers", "x-auth");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With,content-type, Accept , x-auth"
+  );
+
+  next();
+});
+
+app.use("/api/user", userRouter);
+app.use("/api/project", projectRouter);
+app.use("/api/oauth", OAuthRouter);
+module.exports = app;
